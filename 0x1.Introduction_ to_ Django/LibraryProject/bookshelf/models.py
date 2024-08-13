2c1a262d4cf08a5033beacc56f6572201242e7e2
Kam Ephrem <kamnoah88@gmail.com>
2024-08-13 10:15:31 +0100

    edited admin interface

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
@@ -6,3 +6,5 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 # Create your models here.
+    def __str__(self):
+        return f"{self.title} {self.author} {self.publication_year}"
