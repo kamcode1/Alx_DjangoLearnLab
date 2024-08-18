8ce7aa51dfc45b08439e6cb1d1a2efb5af30034a
Kam Ephrem <kamnoah88@gmail.com>
2024-08-18 22:01:36 +0100

    views and urls added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book  # Ensure Library is imported
 from .models import Library
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
