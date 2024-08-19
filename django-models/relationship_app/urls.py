17ddf47ab42ea3f1afc16f4118b97f5df1298d43
Kam Ephrem <kamnoah88@gmail.com>
2024-08-19 13:39:56 +0100

    Authentication implemented

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, Registration, CustomLoginView, CustomLogoutView
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', Registration.as_view(), name='register'),
+    path('books/', views.list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    
+    # Django's built-in views
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    
+    # Custom views
+    path('custom_login/', views.CustomLoginView.as_view(), name='custom_login'),
+    path('custom_logout/', views.CustomLogoutView.as_view(), name='custom_logout'),
+
+    # Registration view (custom)
+    path('register/', views.Registration.as_view(), name='register'),
 ]
