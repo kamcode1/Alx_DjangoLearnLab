3483fcda6138da51d396a3bc41cf2f2a541d086e
Kam Ephrem <kamnoah88@gmail.com>
2024-08-19 16:01:26 +0100

    Authentication implemented

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-class Register(CreateView):
+class register(CreateView):
     form_class  = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = 'relationship_app/register.html'
