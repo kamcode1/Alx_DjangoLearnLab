4d0eb1bd3cb7a5752ad5a41199345758f03fe0ce
Kam Ephrem <kamnoah88@gmail.com>
2024-08-21 13:15:00 +0100

    Meta class added, Persmissions management

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -13,9 +13,16 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.title
+    class Meta:
+        permissions = [
+            ("can_add_book","can add a book"), 
+            ("can_change_book", "can change a book"), 
+            ("can_delete_book", "can delete a book"),
+        ]
+
+    
 
 class Library(models.Model):
     name = models.CharField(max_length=255)
