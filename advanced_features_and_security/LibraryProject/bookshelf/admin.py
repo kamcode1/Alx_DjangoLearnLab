7bf48133516467706edc9244f619d532e2899774
Kam Ephrem <kamnoah88@gmail.com>
2024-08-24 21:07:52 +0100

    Custom admin added

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Book
-# Register your models here.
-#class BookAdmin(admin.ModelAdmin):
-    #list_display = ('title', 'author', 'publication_year')
-    #list_filter = ('author', 'publication_year')
-    #search_fields = ('title', 'author')
-admin.site.register(Book)
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
+
+class UserAdmin(BaseUserAdmin):
+    # Define the fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, UserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -37,3 +37,5 @@ class CustomUser(AbstractUser):
 
 
 
+
+
