bdabdadd37dd96262099e291aabe93b4b59c66ee
Kam Ephrem <kamnoah88@gmail.com>
2024-08-29 20:28:22 +0100

    Error

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+# edited the import statement
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework import generics
+from .models import Book
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
