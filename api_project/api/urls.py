01b8fc800dc0e300015abcce7762b5ac83ade7eb
Kam Ephrem <kamnoah88@gmail.com>
2024-09-05 10:57:40 +0300

    edited urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -11,15 +11,16 @@ router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
+     # New URL pattern for the BookList view
+    path('books-list/', BookList.as_view(), name='book-list'),  # Add new URL pattern for the BookList view
+   
     # Existing URL for the old view
     path('api/books-old/', BookListCreateAPIView.as_view(), name='book_list_create_view'),
     
     # Include URLs managed by the router for the new BookViewSet
     path('api/', include(router.urls)),
 
-    # New URL pattern for the BookList view
-    path('books-list/', BookList.as_view(), name='book-list'),  # Add new URL pattern for the BookList view
-    
+   
     # Token authentication endpoint
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
