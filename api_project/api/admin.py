ae803c7943e4f9b3f8e3eda2d80bab425b355c53
Kam Ephrem <kamnoah88@gmail.com>
2024-09-06 20:18:38 +0300

    Authentication and Permissions added

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+# we have to look into it
 
 # Register your models here.
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api'
 ]
 
@@ -123,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
