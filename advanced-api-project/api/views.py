4ef135b0aa6eddac84375c1a0c1fc4ec7452b9e4
Kam Ephrem <kamnoah88@gmail.com>
2024-09-08 23:54:59 +0300

    generic views added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import response
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import IsAuthenticated, AllowAny
 
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+    def perform_create(self, serializer):
+        # Custom logic here if needed
+        serializer.save()
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    quertset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    def perform_update(self, serializer):
+        # Custom logic here if needed
+        serializer.save()
 # Create your views here.
