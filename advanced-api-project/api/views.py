9c0962c42d583aa5c91de0b9796b8e356ff9dab2
Kam Ephrem <kamnoah88@gmail.com>
2024-09-08 23:58:22 +0300

    generic views added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,11 @@
 from rest_framework import status
 from rest_framework import response
 from rest_framework import generics
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import IsAuthenticated, AllowAny
-
+from .forms import BookForm
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -21,3 +22,14 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
         # Custom logic here if needed
         serializer.save()
 # Create your views here.
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+
+class BookDeleteView(DeleteView):
+    model = Book
+    success_url = '/success-url/'  # Redirect after delete
