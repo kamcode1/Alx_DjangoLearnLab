29e9ee8213f9ed08308fdb1c5ac440351ae93feb
Kam Ephrem <kamnoah88@gmail.com>
2024-09-13 22:37:12 +0300

    error edited

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
+
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 class BookCreateView(APIView):
     def post(self, request, *args, **kwargs):
