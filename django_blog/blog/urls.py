209695975ad08d307f145e48ea977b90e75b2323
Kam Ephrem <kamnoah88@gmail.com>
2024-09-20 14:07:40 +0300

    edited urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 
     #comment URLS
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name = 'comment-delete'),
 ]
