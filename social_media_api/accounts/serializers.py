ba8e80c31ef5cc58540d639d967fbcf7c4346d4f
Kam Ephrem <kamnoah88@gmail.com>
2024-09-22 23:50:08 +0300

    edited

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # Explicitly define password as CharField
 
     class Meta:
         model = get_user_model()
