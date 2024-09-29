e7ac6c5a583a0a2e6dd1b600a50affbd008d6bee
Kam Ephrem <kamnoah88@gmail.com>
2024-09-29 22:39:06 +0300

    edited

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,9 +3,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .models import User
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
+from .models import User
 from .serializers import RegisterSerializer, UserSerializer
 
 class RegisterView(generics.CreateAPIView):
@@ -44,19 +44,14 @@ class LoginView(generics.GenericAPIView):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def follow_user(request, user_id):
-    try:
-        user_to_follow = User.objects.get(id=user_id)
-        request.user.following.add(user_to_follow)
-        return Response({'message': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    user_to_follow = get_object_or_404(User, id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({'message': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def unfollow_user(request, user_id):
-    try:
-        user_to_unfollow = User.objects.get(id=user_id)
-        request.user.following.remove(user_to_unfollow)
-        return Response({'message': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    user_to_unfollow = get_object_or_404(User, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({'message': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
